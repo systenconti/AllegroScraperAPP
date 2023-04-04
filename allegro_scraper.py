9330c5ff1ce7bf94b3b21839b712387501a6a8e8
Bartosz Sowa <bartoszsowa.bsi@gmail.com>
2023-04-05 00:01:08 +0200

    Make code more readable

--- allegro_scraper.py
+++ allegro_scraper.py
@@ -12,7 +12,6 @@ browser = webdriver.Firefox(
 
 
 browser.implicitly_wait(5)
-
 browser.get('https://allegro.pl/?source=allegro.com')
 
 
@@ -23,17 +22,19 @@ browser.find_element(
 sleep(2)
 search_box = browser.find_element(
     by='xpath', value="/html/body/div[2]/div[2]/div/div/div/div/div/div/div/div/div[3]/header/div[1]/div/div/div/form/input")
-
 search_box.send_keys("alpha spirit wild fish 12kg")
+
 
 search_button = browser.find_element(
     by='xpath', value='/html/body/div[2]/div[2]/div/div/div/div/div/div/div/div/div[3]/header/div[1]/div/div/div/form/button')
 search_button.click()
 
+
 select = Select(browser.find_element(value="allegro.listing.sort"))
 # select by value
 select.select_by_value('p')
 sleep(4)
+
 # get particular offers
 offers_list = []
 index = 5
@@ -49,17 +50,25 @@ for _ in range(10):
     price = browser.find_element(
         by='xpath', value=f'//*[@id="search-results"]/div[5]/div/div/div[1]/div/div/section[2]/article[{index}]/div/div/div[2]/div[2]/div/div/span').text
     index += 1
+
     offer_specs.append(offers_name)
     offer_specs.append(link)
     offer_specs.append(price)
     offers_list.append(offer_specs)
 
 print(offers_list)
+
+
 with open('offers.txt', 'w', encoding="UTF-8") as file:
-    file.writelines([str(offer).replace("[", "").replace("]", "") + "\n" for offer in offers_list])
+    file.writelines([str(offer).replace("[", "").replace("]",
+                    "") + "\n" for offer in offers_list])
+
 
 browser.save_full_page_screenshot("karmy.png")
 
+
 send_email()
 print("Email was sent")
+
+
 browser.quit()
